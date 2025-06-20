885b31bec464ce0dfe52bfb1804461348c590f3a
lorrayne-amorim <lorrayne_afdc@hotmail.com>
2025-06-20 09:44:50 -0300

    Teste ok

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,15 +23,15 @@ redisClient.on('error', (err: any) => console.log('Redis Client Error', err));
 })();
 
 app.use(cors({
-  origin: 'http://localhost:8080', // Frontend URL
-  methods: ['GET', 'POST'],
-  credentials: true
+    origin: 'http://localhost:8080', // Frontend URL
+    methods: ['GET', 'POST'],
+    credentials: true
 }));
 app.use(express.json());
 app.use('/api', subscribeRoutes);
 
 // Define uma URI padrão caso não exista no .env
-const mongoUri = process.env.MONGO_URI || 'mongodb://root:example@database:27017/esx?authSource=admin';
+const mongoUri = process.env.DATABASE_URL || 'mongodb://root:example@database:27017/esx?authSource=admin';
 console.log('Tentando conectar ao MongoDB com URI:', mongoUri);
 
 // Tenta conectar ao MongoDB
@@ -39,12 +39,12 @@ mongoose.connect(mongoUri)
     .then(() => console.log('MongoDB conectado'))
     .catch((err: any) => {
         console.error('Erro ao conectar no MongoDB com a URI principal:', err);
-        
+
         // Só tenta a URI alternativa se a original existir
         if (mongoUri && mongoUri.includes('database:')) {
             const fallbackUri = mongoUri.replace('database:', 'localhost:');
             console.log('Tentando conexão alternativa com:', fallbackUri);
-            
+
             mongoose.connect(fallbackUri)
                 .then(() => console.log('MongoDB conectado usando URI alternativa'))
                 .catch((fallbackErr: any) => console.error('Erro ao conectar no MongoDB com URI alternativa:', fallbackErr));
